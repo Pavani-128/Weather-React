0abf3ae3d24fcf4b3d51ca7a404702aa6bbfc18a
Pavani-128 <168661778+Pavani-128@users.noreply.github.com>
2024-07-22 18:31:06 +0530

    weather changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather1 from './components/Weather1'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+     <Weather1 />
+
+    </div>
+  )
+}
+
+export default App
